ac76124eddd55714cf12072d28ccd56edb216932
Bartek B <bielawb@CentGUI.monad.net>
2017-10-27 11:36:29 +0800

    Updated version of scripts

--- 01_demo.sh
+++ 01_demo.sh
@@ -7,7 +7,7 @@ cd ~
 p "${GREEN}# First, we need to be able to use pip"
 pe 'sudo yum -y install epel-release'
 p "${GREEN}# Next, we can install packages required for pywinrm w/ kerberos"
-pe 'sudo yum -y install pip gcc krb5-devel krb5-workstation python-devel'
+pe 'sudo yum -y install python-pip gcc krb5-devel krb5-workstation python-devel'
 p "${GREEN}# Time to install pywinrm!"
 pe 'sudo pip install pywinrm[kerberos]'
 p "${GREEN}# Our scripts - time to see them!"
--- 03_improve.sh
+++ 03_improve.sh
@@ -6,21 +6,21 @@ TYPE_SPEED=50
 clear
 cd ~/PSConfAsia/Scripts
 p "${GREEN}# Using SSL - we have to change endpoint"
-pe 'cat bin/withSsl.py'
+pe 'cat ./withSsl.py'
 p "${GREEN}# The problem: CA is not easy to configure... "
-pe './withSsl ls'
+pe './withSsl.py ls'
 wait
 clear
 p "${GREEN}# Three options:"
 p "${GREEN}# -- ignore cert validation"
 p "${GREEN}# -- modify requests and add our CA"
 p "${GREEN}# -- modify pywinrm to allow seperate cert"
-pe './httpsWinRM ls'
+pe './httpsWinRM.py ls'
 wait
 clear
 p "${GREEN}# To make it more convenient - we can wrap python around it and expose as a single command"
 pe 'cat adda.py | less'
-pe 'adda --help'
+pe './adda.py --help'
 p "${GREEN}# Let's see it in action!"
-pe 'adda -n PSRocks -i 192.168.7.150'
+pe './adda.py -n PSRocks -i 192.168.7.150'
 pe 'nslookup PSRocks.monad.net 192.168.7.1'
